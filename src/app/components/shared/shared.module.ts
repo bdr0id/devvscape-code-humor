3963660fff5c51c62b676b250ccc22d17d5554a8
l00pinfinity <collinskipkemboi001@gmail.com>
2025-06-26 14:58:00 +0400

    feat(performance): optimize production build

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { IonicModule } from '@ionic/angular';
 import { ErrorBannerComponent } from './components/error-banner/error-banner.component';
 import { LazyImgDirective } from 'src/app/core/directives/lazy-img.directive';
 import { DateAgoPipe } from 'src/app/core/pipes/date-ago.pipe';
+import { QuicklinkModule } from 'ngx-quicklink';
 
 @NgModule({
   declarations: [DateAgoPipe, LazyImgDirective,ErrorBannerComponent,],
-  imports: [CommonModule,IonicModule,],
-  exports: [DateAgoPipe, LazyImgDirective,ErrorBannerComponent,],
+  imports: [CommonModule,IonicModule, QuicklinkModule],
+  exports: [DateAgoPipe, LazyImgDirective,ErrorBannerComponent, QuicklinkModule],
 })
 export class SharedModule { } 
